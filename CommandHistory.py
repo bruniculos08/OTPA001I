4014fdcb1e35eb455bd345c14364be7fdfb49d2e
Bruno Rafael dos Santos <62629549+bruniculos08@users.noreply.github.com>
2022-08-25 09:26:33 -0300

    update

--- CommandHistory.py
+++ CommandHistory.py
@@ -6,19 +6,41 @@ Code your solution here
 Escriba su solución aquí
 '''
 
+# Função que retorna o index da última ocorrência de um elemento em uma lista
+
+def lastOcurrence(list, item):          # O(n)
+    last = -1
+    if(list == None):                   
+        return -1
+    for i in range(len(list)):
+        if(list[i] == item):
+            last = i
+    return last
+
+
 while(1):
     N = int(input("Enter N: "))
     if(N == 0): break
-    list = map(int, input().split()) #split without arg uses space bar
-    result = 0
-    extra = 0
-    for pos in list:
-        result += (pos + extra)
-        if(pos > 1): extra += 1
+    list = map(int, input().split())    # Split without arg uses space bar | O(n)
+    result, extra = 0, 0
+
+    newList = []
+
+    for pos in list:                        # O(n.m) onde m = len(list) e n = len(newList)
+        aux = lastOcurrence(newList, pos)   # O(n)              
+        if(aux != -1):                      # O(1)
+            result += len(newList) - aux    # O(n)
+        else:
+            result += (pos + extra)
+
+        newList.append(pos)                 # O(1)
+        if(pos >= 1): extra += 1
+
     print(result)
 
 #4
 #1 3 1 3
 
-# a b c d e
-# a b c d e
+# a b c d e 
+# a b c d e e
+# a b c d e e c
