a893bbbe932be0d23664fdd954ec14a37f454915
Bruno Rafael dos Santos <bruniculos2014@gmail.com>
2022-12-02 00:50:21 -0300

    update

--- k-Means/kMeans (N dimensions).py
+++ k-Means/kMeans (N dimensions).py
@@ -8,15 +8,21 @@ from mpl_toolkits import mplot3d
 # This function receives the number of clusters k, the data points (it can have any dimension)
 def kMeans(k, data, iterations=1):
     unziped_data = list(zip(*data))
-    centroids = [(random.uniform(min(unziped_data[0]), max(unziped_data[0])), random.uniform(min(unziped_data[1]), max(unziped_data[1]))) for _ in range(k)]
+    dimensions = len(unziped_data)
+    centroids = []
+    for i in range(k):
+        coord = []
+        for j in range(dimensions):
+            coord.append(random.uniform(min(unziped_data[j]), max(unziped_data[j])))
+        centroids.append(tuple(coord))
 
     for _ in range(iterations):
         new_centroids = centroids
-        centroids, data_group_list = calcMeans(data, centroids)
+        centroids, data_group_list = calcMeans(data, centroids, dimensions)
     return new_centroids, data_group_list
 
     
-def calcMeans(data, centroids):
+def calcMeans(data, centroids, dimensions):
     data_group_list = [[] for _ in range(len(centroids))]
 
     for point in data:
@@ -29,13 +35,12 @@ def calcMeans(data, centroids):
 
     new_centroids = []
     for i, group in enumerate(data_group_list):
+        new_coords = [0 for _ in range(dimensions)]
         if len(group) > 0:
-            new_x = 0
-            new_y = 0
             for point in group:
-                new_x += point[0]/len(group)
-                new_y += point[1]/len(group)
-            new_centroids.append((new_x, new_y))
+                for i, coord in enumerate(point):
+                    new_coords[i] += coord/len(group)
+            new_centroids.append(tuple(new_coords))
         else:
             new_centroids.append(centroids[i])
 
@@ -62,4 +67,21 @@ if __name__ == "__main__":
         for (x, y) in data_group_list[i]:
             plt.scatter(x, y, color = hexadecimal)
     plt.savefig("C:\\Users\\bruni\\OneDrive\\Documentos\\GitHub\\OTPA001I\\k-Means\\Exemplo01 (k-Means).png")
+    plt.close()
+
+    #Exemple02 (3d plot):
+    X = [random.uniform(1, 200) for _ in range(200)]
+    Y = [random.uniform(1, 200) for _ in range(200)]
+    Z = [random.uniform(1, 200) for _ in range(200)]
+    data = list(zip(X, Y, Z))
+
+    centroids, data_group_list = kMeans(4, data, 1)
+    
+    for i, (x_c, y_c, z_c) in enumerate(centroids):
+        hexadecimal = ["#"+''.join([random.choice('ABCDEF0123456789') for i in range(6)])]
+        ax = plt.axes(projection = '3d')
+        ax.scatter3D(x_c, y_c, z_c, color = hexadecimal, marker='o', s = 100, alpha = 0.5)
+        for (x, y, z) in data_group_list[i]:
+            ax.scatter3D(x, y, z, color = hexadecimal)
+    plt.savefig("C:\\Users\\bruni\\OneDrive\\Documentos\\GitHub\\OTPA001I\\k-Means\\Exemplo02 (k-Means).png")
     plt.close()
